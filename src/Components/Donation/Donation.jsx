b84ab74003307d5ab42db88ce57eed839a5db9de
Shahriar Ahmmed <shahriarzemam28@gmail.com>
2023-09-25 23:48:46 +0600

    see all button added with functionality

--- src/Components/Donation/Donation.jsx
+++ src/Components/Donation/Donation.jsx
@@ -2,32 +2,33 @@ import { useEffect, useState } from "react";
 import DonationDetails from "../DonationDetails/DonationDetails";
 
 const Donation = () => {
-    const [donation,setDonation]=useState([]);
-    const [donateItem,setDonateItem]=useState("")
-    const [display,setDisplay]=useState(false)
-    useEffect(()=>{
+    const [dataLength, setDatalength] = useState(4)
+    const [donation, setDonation] = useState([]);
+    const [donateItem, setDonateItem] = useState("")
+    useEffect(() => {
         const item = JSON.parse(localStorage.getItem('donate'))
-        if(item){
+        if (item) {
             setDonation(item)
         }
-        else{
+        else {
             setDonateItem('No Donations')
         }
-    },[])
+    }, [])
     return (
         <div>
-            { donateItem ? <p className="text-center text-[#FF444A] text-3xl lg:text-5xl font-bold mt-20">No Donations Yet!!!</p> :
-            <div>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 max-w-7xl mx-auto gap-5 mt-10">
-                    {
-                        display ? donation.map(donate=><DonationDetails key={donate.id} donate={donate}></DonationDetails> ):
-                        donation.slice(0,4).map(donate => <DonationDetails key={donate.id} donate={donate}></DonationDetails>)
-                    }
+            {donateItem ? <p className="text-center text-[#FF444A] text-3xl lg:text-5xl font-bold mt-20">No Donations Yet!!!</p> :
+                <div>
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 max-w-7xl mx-auto gap-5 mt-10">
+                        {
+                            donation.slice(0, dataLength).map(donate => <DonationDetails key={donate.id} donate={donate}></DonationDetails>)
+                        }
+                    </div>
+                    <div className={dataLength >= donation.length ? 'hidden' : ''}>
+                        <div className="flex justify-center mt-5">
+                            <button className="btn btn-accent" onClick={() => setDatalength(donation.length)}>SEE ALL</button>
+                        </div>
+                    </div>
                 </div>
-                <div className="text-center mt-5">
-                    <button className="btn btn-accent" onClick={()=>setDisplay(!display)}>SEE ALL</button>
-                </div>
-            </div>
             }
         </div>
     );
