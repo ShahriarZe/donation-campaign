658ce825bde1007eb9a4b6539dd38716804961eb
Shahriar Ahmmed <shahriarzemam28@gmail.com>
2023-09-26 17:49:20 +0600

    statistics component added with functionality

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 const ErrorPage = () => {
     return (
         <div className="text-center text-5xl font-bold mt-20 text-[#FF444A]">
-            <h2>OOPSS !!!</h2>
+            <h2>404 !!!</h2>
             <h2 className="mt-5">No Pages Found</h2>
            <NavLink to='/'>
            <button className="btn btn-error mt-10" >Go HOME</button>
--- src/Components/Statistics/Statistics.jsx
+++ src/Components/Statistics/Statistics.jsx
@@ -1,15 +1,63 @@
-import { PieChart } from 'react-minimal-pie-chart';
-const Statistics = () => {
-    return (
-        <div className='max-w-xl mx-auto mt-20'>
-            <div>
-            <PieChart
-                data={[
-                    { title: 'One', value: 50, color: '#E38627' },
-                    { title: 'Two', value: 50, color: '#C13C37' },
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react";
+import ApexChart from "react-apexcharts"
 
-                ]}
-            />;
+const Statistics = () => {
+    const [data, setData] = useState([])
+    const [donateId, setDonateId] = useState([])
+    const [percentValue, setPercentValue] = useState(0)
+    console.log(donateId);
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(item => setData(item))
+        const localData = JSON.parse(localStorage.getItem('donate'))
+        if (localData) {
+            setDonateId(localData)
+        }
+    }, [])
+    useEffect(() => {
+        const amount = donateId.reduce((acc, singleId) => {
+            const matchId = data.find((item) => item.id === singleId.id)
+            if (matchId) {
+                return acc + matchId.price
+            }
+            return acc
+        }, 0)
+        if (amount > 0 && data.length > 0) {
+            const totalDonation = data.reduce((acc, donatePrice) => acc + donatePrice.price, 0)
+            const calculateData = (amount / totalDonation) * 100
+            setPercentValue(calculateData)
+        }
+        else {
+            setPercentValue(0)
+        }
+    }, [donateId, data])
+    const userPercentage = 100 - percentValue;
+    const chartData = [{
+        name: 'Your Donation',
+        data: 100 - userPercentage,
+    },
+    {
+        name: 'Total Donations',
+        data: userPercentage,
+    }]
+    const settings = {
+        lebels: chartData.map(item => item.name),
+        legend: {
+            position: 'bottom'
+        }
+
+    }
+    return (
+        <div className=''>
+            <div>
+                <ApexChart
+                    options={settings}
+                    series={chartData.map(item => item.data)}
+                    type="pie"
+                    width={500}
+                ></ApexChart>
             </div>
         </div>
     );
